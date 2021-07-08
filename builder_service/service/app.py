25c4ca99fc31b862957fac4b8ea7555d90656f87
Carlos Millán Soler <carmilso@inf.upv.es>
2021-07-08 09:09:43 +0200

    Lint fix

--- builder_service/service/app.py
+++ builder_service/service/app.py
@@ -62,7 +62,7 @@ def create_job_object(
     )
 
     # XXX: only compatible with Docker Hub at the moment.
-    registry_credentials = f'{"auths":{"https://index.docker.io/v1/":{"auth":"{registry_auth}"}}}
+    registry_credentials = f'{"auths":{"https://index.docker.io/v1/":{"auth":"{registry_auth}"}}}'
 
     init_container = client.V1Container(
         name='credentials',
