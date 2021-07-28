71d5e4f3f9b579118c527edaebe464feabe80b6a
Carlos Millán Soler <carmilso@inf.upv.es>
2021-07-28 09:22:11 +0200

    Add readme to PyPI package

--- chassisml-sdk/setup.py
+++ chassisml-sdk/setup.py
@@ -5,12 +5,17 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='chassisml',
     version='0.0.1',
-    author='Carlos Millan Soler',
+    author='Carlos Millán Soler',
     author_email='cmillan@sciling.com',
     description='Python API client for Chassis.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=['requests'],
