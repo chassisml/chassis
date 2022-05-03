16000f9e083bd4108a81a3f1f70c03a9b616d2bf
saumil-d <saumil.dave@modzy.com>
2022-05-03 12:54:55 -0700

    add versions to sdk deps

--- chassisml_sdk/setup.py
+++ chassisml_sdk/setup.py
@@ -18,9 +18,9 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     python_requires='>=3.6,<3.9',
-    install_requires=['requests','mlflow','numpy','pyyaml','validators','grpc-requests','grpcio>=1.44.0','docker',
-                    'opencv-python-headless','onnxruntime','onnx','mxnet','boto3','xgboost==0.90.0','gluoncv','azureml-core>=1.41.0',
-                    'azureml-automl-runtime','pandas'],
+    install_requires=['requests','mlflow==1.22','numpy==1.19.5','pyyaml','validators','grpc-requests==0.1.0','grpcio>=1.44.0','docker==5.0.3',
+                    'opencv-python-headless==4.5.4.60','onnxruntime==1.8.1','onnx==1.7.0','mxnet==1.8','boto3','xgboost==1.4','gluoncv==0.10.5','azureml-core>=1.41.0',
+                    'azureml-automl-runtime==1.41.0','pandas==1.1.5'],
     url='https://github.com/modzy/chassis/tree/main/chassisml-sdk',
     zip_safe=False,
 )
