200673e8b30a92d6702ce3a2491e520b4f5617f1
saumil-d <83971510+saumil-d@users.noreply.github.com>
2022-06-13 14:27:38 -0700

    dev: use service principal auth

--- chassisml_sdk/chassisml/azureml/utils.py
+++ chassisml_sdk/chassisml/azureml/utils.py
@@ -5,11 +5,14 @@ import tempfile
 import subprocess
 
 def download_model_artifacts(tenant_id,service_principal_id,service_principal_password,
-                            workspace_name,subscription_id,resource_group,model_name,output_dir):
+                    workspace_name,subscription_id,resource_group,model_name,output_dir):
     '''
     Downloads registered Azure ML model artifacts
 
     Args:
+        tenant_id (str): Azure ML tenant id
+        service_principal_id (str): Azure ML service principal id with access to workspace
+        service_principal_password (str): Azure ML service principal password with access to workspace
         workspace_name (str): Azure ML workspace name
         subscription_id (str): Azure ML subscription ID
         resource_group (str): Azure ML resource group
@@ -25,22 +28,25 @@ def download_model_artifacts(tenant_id,service_principal_id,service_principal_pa
             service_principal_id=service_principal_id,
             service_principal_password=service_principal_password
         )
-
+        
     ws = Workspace.get(name=workspace_name,
-               subscription_id=subscription_id,
-               resource_group=resource_group,
-               auth=svc_pr)
+            subscription_id=subscription_id,
+            resource_group=resource_group,
+            auth=svc_pr)
 
     model = Model(ws, model_name)
-    model.download(output_dir)
+    model.download(output_dir,exist_ok=True)
     print("Model downloaded.")
 
-def replicate_remote_env(tenant_id,service_principal_id,service_principal_password,workspace_name,
-                        subscription_id,resource_group,remote_env_name,local_env_name=None):
+def replicate_remote_env(tenant_id,service_principal_id,service_principal_password,
+    workspace_name,subscription_id,resource_group,remote_env_name,local_env_name=None):
     '''
     Replicates remote Azure ML environment locally, requires conda
 
     Args:
+        tenant_id (str): Azure ML tenant id
+        service_principal_id (str): Azure ML service principal id with access to workspace
+        service_principal_password (str): Azure ML service principal password with access to workspace
         workspace_name (str): Azure ML workspace name
         subscription_id (str): Azure ML subscription ID
         resource_group (str): Azure ML resource group
@@ -59,11 +65,11 @@ def replicate_remote_env(tenant_id,service_principal_id,service_principal_passwo
             service_principal_id=service_principal_id,
             service_principal_password=service_principal_password
         )
-
+        
     ws = Workspace.get(name=workspace_name,
-               subscription_id=subscription_id,
-               resource_group=resource_group,
-               auth=svc_pr)
+            subscription_id=subscription_id,
+            resource_group=resource_group,
+            auth=svc_pr)
 
     myenv = Environment.get(workspace=ws, name=remote_env_name)
     yaml_data = myenv.python.conda_dependencies.as_dict()
