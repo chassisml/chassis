bc3a7a6a9de5a7d129558eef5c696a47c237eedc
saumil-d <83971510+saumil-d@users.noreply.github.com>
2022-07-05 13:47:03 -0700

    increment version

--- chassisml_sdk/setup.py
+++ chassisml_sdk/setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='chassisml',
-    version='1.4.3',
+    version='1.4.4',
     author='Carlos Millán Soler',
     author_email='cmillan@sciling.com',
     description='Python API client for Chassis.',
