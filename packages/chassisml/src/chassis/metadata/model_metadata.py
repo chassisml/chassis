f0158ce1cae49c01c5ab78c30f763b80f5d4bc09
Nathan Mellis <nathan@mellis.us>
2023-07-21 08:03:43 -0700

    Added init method to ModelMetadata.

--- packages/chassisml/src/chassis/metadata/model_metadata.py
+++ packages/chassisml/src/chassis/metadata/model_metadata.py
@@ -14,6 +14,16 @@ class ModelMetadata:
     timeout: ModelTimeout
     features: ModelFeatures
 
+    def __init__(self, info: ModelInfo = None, description: ModelDescription = None, inputs: List[ModelInput] = None, outputs: List[ModelOutput] = None, resources: ModelResources = None, timeout: ModelTimeout = None, features: ModelFeatures = None):
+        md = ModelMetadata.default()
+        self.info = info if info is not None else md.info
+        self.description = description if description is not None else md.description
+        self.inputs = inputs if inputs is not None else md.inputs
+        self.outputs = outputs if outputs is not None else md.outputs
+        self.resources = resources if resources is not None else md.resources
+        self.timeout = timeout if timeout is not None else md.timeout
+        self.features = features if features is not None else md.features
+
     @classmethod
     def default(cls: Type[T]) -> T:
         md = cls()
