9cca8bd2f9eea6f2882906e836271a8afe3993ae
Nathan Mellis <nathan@mellis.us>
2023-08-08 15:36:08 -0700

    First stab at re-implementing the kserve server.

--- packages/chassisml/src/chassis/runtime/model_runner.py
+++ packages/chassisml/src/chassis/runtime/model_runner.py
@@ -1,11 +1,40 @@
+import os
 import json
-from typing import List, Mapping
+import cloudpickle
+from typing import List, Mapping, Type, TypeVar, Union
 
 from chassis.typing import PredictFunction
 from .numpy_encoder import NumpyEncoder
+from .constants import PACKAGE_DATA_PATH, PYTHON_MODEL_KEY, python_pickle_filename_for_key
+
+T = TypeVar("T", bound="ModelRunner")
+
+
+def batch(items, size):
+    for i in range(0, len(items), size):
+        yield items[i:i + size]
 
 
 class ModelRunner:
+
+    @classmethod
+    def load(cls: Type[T]) -> Union[T, None]:
+        try:
+            # If this is the first time calling the `Status` route, then attempt to load the model
+            filename = python_pickle_filename_for_key(PYTHON_MODEL_KEY)
+            with open(os.path.join(PACKAGE_DATA_PATH, filename), "rb") as f:
+                modules = cloudpickle.load(f)
+            model: T = modules[PYTHON_MODEL_KEY]
+            if model is None:
+                raise "Model not found"
+            message = "Model Initialized Successfully."
+            print(message)
+            return model
+        except Exception as e:
+            # If there is a problem in loading the model, catch it and report the error
+            message = "Model Failed to Initialize."
+            print(f"{message} Error: {e}")
+            return None
 
     def __init__(self, predict_fn: PredictFunction, batch_size: int = 1, is_legacy_fn=False):
         self.predict_fn = predict_fn
@@ -35,8 +64,12 @@ class ModelRunner:
         return outputs
 
     def _predict_batch(self, inputs: List[Mapping[str, bytes]]) -> List[Mapping[str, bytes]]:
-        # TODO - split inputs into groups of self.batch_size
-        return self.predict_fn(inputs)
+        outputs = []
+        # Split inputs into groups of self.batch_size
+        batches = batch(inputs, self.batch_size)
+        for b in batches:
+            outputs.extend(self.predict_fn(b))
+        return outputs
 
     def _predict_legacy(self, inputs: List[Mapping[str, bytes]]) -> List[Mapping[str, bytes]]:
         if self.batch_size == 1:
@@ -47,7 +80,10 @@ class ModelRunner:
             return outputs
         else:
             adjusted_inputs = [input_item["input"] for input_item in inputs]
-            # TODO - split inputs into groups of self.batch_size
-            outputs = self.predict_fn(adjusted_inputs)
+            # Split inputs into groups of self.batch_size
+            outputs = []
+            batches = batch(adjusted_inputs, self.batch_size)
+            for b in batches:
+                outputs.extend(self.predict_fn(b))
             adjusted_outputs = [{"results.json": json.dumps(o, separators=(",", ":"), cls=NumpyEncoder).encode()} for o in outputs]
             return adjusted_outputs
