81a132a1e094713da337a885466ccc6a5844abf6
bmunday3 <bwmunday@gmail.com>
2023-08-11 15:49:32 -0400

    Updated sample inference code

--- examples-new/getting-started/inference.py
+++ examples-new/getting-started/inference.py
@@ -4,6 +4,6 @@ from chassis.guides import DigitsSampleData
 with OMIClient("localhost", 45000) as client:
     status = client.status()
     print(f"Status: {status}")
-    res = client.run([{'input': DigitsSampleData}])
+    res = client.run(DigitsSampleData)
     result = res.outputs[0].output["results.json"]
     print(f"Result: {result}")
