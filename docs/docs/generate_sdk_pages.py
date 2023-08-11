7a46caee7f7b2d215611aa1e3bb4ce30972e988f
Nathan Mellis <nathan@mellis.us>
2023-08-11 15:53:09 -0700

    Update mkdocs to auto-generate SDK documentation.

---
+++ docs/docs/generate_sdk_pages.py
@@ -0,0 +1,40 @@
+"""Generate all the SDK Reference documentation at build time."""
+
+from pathlib import Path
+
+import mkdocs_gen_files
+
+nav = mkdocs_gen_files.Nav()
+
+CHASSIS_SRC_PATH = "../packages/chassisml/src"
+for path in sorted(Path(CHASSIS_SRC_PATH).rglob("*.py")):
+    module_path = path.relative_to(CHASSIS_SRC_PATH).with_suffix("")
+    doc_path = path.relative_to(CHASSIS_SRC_PATH).with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
+
+    parts = list(module_path.parts)
+
+    # Skip the builder templates directory.
+    try:
+        for p in parts:
+            if p == "templates":
+                raise ValueError("break!")
+    except ValueError:
+        continue
+
+    if parts[-1] == "__init__":
+        # parts = parts[:-1]
+        continue
+    elif parts[-1] == "__main__":
+        continue
+
+    nav[parts] = doc_path.as_posix()
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
+
+with mkdocs_gen_files.open("reference/navigation.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
