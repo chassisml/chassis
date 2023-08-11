004cdc5736f840c0b10094d0838affb5516e6bb5
Nathan Mellis <nathan@mellis.us>
2023-08-11 09:43:20 -0700

    Removed the now-obsolete files.

--- chassisml_sdk/chassisml/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Chassis Python API Client."""
-
-name = 'chassisml'
-__version__ = '1.4.13'
-
-from .chassisml import ChassisClient,ChassisModel
--- service/flavours/mlflow/interfaces/modzy/model_lib/README.md
+++
@@ -1,2 +0,0 @@
-# model_lib
-holds the baseline model used for chassis
