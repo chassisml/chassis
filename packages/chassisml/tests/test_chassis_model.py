7d771710651324b306fc61d33e5f2799f51ea0cf
Nathan Mellis <nathan@mellis.us>
2023-10-26 08:52:39 -0700

    Make `Buildable` fields instance variables. (#145)

--- packages/chassisml/tests/test_chassis_model.py
+++ packages/chassisml/tests/test_chassis_model.py
@@ -37,4 +37,22 @@ def test_adding_pip_requirements_from_conda_env(echo_predict_function):
     model.parse_conda_env(env)
     assert len(model.requirements.symmetric_difference(["scikit-learn", "numpy", "chassisml"])) == 0
 
+
+def test_each_model_instance_has_own_default_values(echo_predict_function):
+    model = ChassisModel(echo_predict_function)
+    assert model.metadata.model_name == ""
+    assert model.metadata.model_version == ""
+    assert model.metadata.has_inputs() is False
+    model.metadata.model_name = "Test Model"
+    model.metadata.model_version = "1.0.1"
+    model.metadata.add_input("input", ["text/plain"])
+    assert model.metadata.model_name == "Test Model"
+    assert model.metadata.model_version == "1.0.1"
+    assert model.metadata.has_inputs() is True
+
+    model2 = ChassisModel(echo_predict_function)
+    assert model2.metadata.model_name == ""
+    assert model2.metadata.model_version == ""
+    assert model2.metadata.has_inputs() is False
+
 # Verify that the fields in the metadata are updated based on the values provided during `prepare_context`.
